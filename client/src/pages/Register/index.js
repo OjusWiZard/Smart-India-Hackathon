6e4997ca8200b2aed8e47467fc15f63ec13cabe5
paarth17032000 <paarth17032000@gmail.com>
2022-03-05 15:46:00 +0530

    feat: register assets
    Signed-off-by: paarth17032000 <paarth17032000@gmail.com>

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { ReactComponent as FileIcon } from "../../assets/icons/file-icon.svg";
+import { ReactComponent as Certisetu } from "../../assets/heading-vectors/CERTISETU.svg";
 
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+      <div className="bg-primary-400">
+        <div className="py-[90px] px-[100px]">
+          <div className="flex items-center justify-start">
+            <FileIcon />
+            <Certisetu className="ml-3" />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">1</div>
+    </div>
+  );
 }
