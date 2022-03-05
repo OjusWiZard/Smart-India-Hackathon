c79ee442539b5ee7e76b6a80a3bad17e3058a764
OjusWiZard <ojuswimail@gmail.com>
2022-03-06 02:43:38 +0530

    added user model with otp api
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authy OTP Verification
+
+AUTHY_KEY = os.environ.get('AUTHY_KEY')
