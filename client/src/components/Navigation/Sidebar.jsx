d82b55d25b58157c3a15b32a024f0721868bdc53
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-06 12:21:36 +0530

    fix: sidebar
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- client/src/components/Navigation/Sidebar.jsx
+++ client/src/components/Navigation/Sidebar.jsx
@@ -1,49 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as FileIconRed } from "../../assets/icons/file-icon-red.svg";
 import { ReactComponent as ScholarshipIcon } from "../../assets/icons/scholarship-icon.svg";
 import { ReactComponent as ProfileIcon } from "../../assets/icons/profile-icon.svg";
 import { ReactComponent as HomeIcon } from "../../assets/icons/home.svg";
 
 const Sidebar = () => {
   return (
-    <>
-      <div className="max-w-xs min-h-screen border-r border-[#FBF8FE]">
-        <div className="pt-9 pl-12">
-          <FileIconRed />
-        </div>
-        <div className="space-y-7 mt-16">
-          <Link as="div" to="/dashboard">
-            <div className="px-4 hover:text-primary-dark hover:border-l-4 hover:border-secondary-dark">
-              <div className="rounded-lg cursor-pointer hover:bg-primary-light py-2">
-                <div className="flex items-center justify-start pr-20">
-                  <HomeIcon className="ml-8 mr-3" />
-                  <div className="font-normal ">Home</div>
-                </div>
-              </div>
+    <aside className="w-72 pt-16" aria-label="Sidebar">
+      <div className="overflow-y-auto py-4 px-3 bg-white">
+        <ul className="space-y-2">
+          <Link as="li" to="/dashboard">
+            <div className="flex items-center p-4 text-base font-normal hover:text-primary-dark hover:border-l-4 hover:border-secondary-dark rounded-lg cursor-pointer hover:bg-primary-light">
+              <HomeIcon className="mr-3" />
+              <span className="font-normal">Dashboard</span>
             </div>
           </Link>
-          <Link as="div" to="/scholarships">
-            <div className="px-4 hover:text-primary-dark hover:border-l-4 hover:border-secondary-dark">
-              <div className="rounded-lg cursor-pointer hover:bg-primary-light py-2">
-                <div className="flex items-center justify-start pr-20">
-                  <ScholarshipIcon className="ml-8 mr-3" />
-                  <div className="font-normal ">Scholarship</div>
-                </div>
-              </div>
+          <Link as="li" to="/scholarships">
+            <div className="flex items-center p-4 text-base font-normal hover:text-primary-dark hover:border-l-4 hover:border-secondary-dark rounded-lg cursor-pointer hover:bg-primary-light">
+              <ScholarshipIcon className="mr-3" />
+              <span className="flex-1 font-normal">Scholarship</span>
             </div>
           </Link>
-          <div className="px-4 hover:text-primary-dark hover:border-l-4 hover:border-secondary-dark">
-            <div className="rounded-lg cursor-pointer hover:bg-primary-light py-2 ">
-              <div className="flex items-center justify-start pr-20">
-                <ProfileIcon className="ml-8 mr-3" />
-                <div className="font-normal ">Profile</div>
-              </div>
+          <Link as="li" to="/scholarships">
+            <div className="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+              <ProfileIcon className="mr-3" />
+              <span className="flex-1 whitespace-nowrap">Profile</span>
             </div>
-          </div>
-        </div>
+          </Link>
+        </ul>
       </div>
-    </>
+    </aside>
   );
 };
 
