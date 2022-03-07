72ca5b4540c1eb137d6e6757e3060420ade15895
Shivam Singh <shivm.0701@gmail.com>
2022-03-07 10:48:42 +0530

    feat: added twilio verification

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers["x-auth-token"];
     const token = authHeader && authHeader.split(" ")[1];
     if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) return res.status(403).send({ result: false, message: "Unauthorized User" });
+        const user = User.findOne({ phone: payload.phone });
         req.user = user;
         next();
     });
