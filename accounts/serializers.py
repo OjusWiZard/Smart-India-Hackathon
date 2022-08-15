cacaa968b06d760c0e1d29707a249843909c0145
OjusWiZard <ojuswimail@gmail.com>
2022-08-16 04:25:19 +0530

    rename UserSerializer
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import User
 
 
-class User_Serializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["email", "full_name", "contact_no"]
