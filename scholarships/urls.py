9cdf6f4aecc7222a1bb636ac3760248cef5801f6
OjusWiZard <ojuswimail@gmail.com>
2022-08-16 03:41:07 +0530

    wired up the scholarship urls
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- scholarships/urls.py
+++ scholarships/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .views import LogicViewSet, ScholarshipViewSet, ApplicationViewSet, EligibilityViewSet
+
+routers = routers.DefaultRouter()
+routers.register("logic", LogicViewSet)
+routers.register("scholarship", ScholarshipViewSet)
+routers.register("application", ApplicationViewSet)
+routers.register("eligibility", EligibilityViewSet)
 
 urlpatterns = [
-    # path("", include(routers.urls)),
+    path("", include(routers.urls)),
 ]
