b47e6b873e2a073af623277c3bcabf49c61af1e4
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-08-19 05:33:07 +0530

    fix: sidebar
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- test2/src/components/Navigation/Sidebar.jsx
+++ test2/src/components/Navigation/Sidebar.jsx
@@ -31,29 +31,31 @@ const Sidebar = () => {
 	];
 	return (
 		<aside className="w-60">
-			<div className="h-[80px] py-6 px-9">
-				<FileIcon />
-			</div>
-			<div className="overflow-hidden py-12 px-4">
-				<ul className="space-y-2">
-					{links.map((linkItem) => {
-						const { link, icon, name } = linkItem;
-						return (
-							<NavLink
-								as="li"
-								to={link}
-								className={({ isActive }) =>
-									isActive
-										? "flex items-center p-4 text-base font-normal text-primary-dark rounded-lg cursor-pointer bg-primary-light"
-										: "flex items-center p-4 text-[#9C9C9C] text-base font-normal hover:text-primary-dark rounded-lg cursor-pointer hover:bg-primary-light"
-								}
-							>
-								{icon}
-								<span className="font-normal">{name}</span>
-							</NavLink>
-						);
-					})}
-				</ul>
+			<div className="fixed">
+				<div className="h-[80px] py-6 px-9">
+					<FileIcon />
+				</div>
+				<div className="overflow-hidden py-12 px-4">
+					<ul className="space-y-2">
+						{links.map((linkItem) => {
+							const { link, icon, name } = linkItem;
+							return (
+								<NavLink
+									as="li"
+									to={link}
+									className={({ isActive }) =>
+										isActive
+											? "flex items-center p-4 text-base font-normal text-primary-dark rounded-lg cursor-pointer bg-primary-light"
+											: "flex items-center p-4 text-[#9C9C9C] text-base font-normal hover:text-primary-dark rounded-lg cursor-pointer hover:bg-primary-light"
+									}
+								>
+									{icon}
+									<span className="font-normal">{name}</span>
+								</NavLink>
+							);
+						})}
+					</ul>
+				</div>
 			</div>
 		</aside>
 	);
